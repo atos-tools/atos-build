b8948d576874bae6c86dc41805bcfce224614fbf
Rémi Duraffort <remi.duraffort@st.com>
2013-05-16 11:09:42 +0200

    setenv: improve visual aspect inspired by venv
    When sourcing setenv.sh, we now:
     * save PATH, PYTHONPATH and PSE1 in _OLD_ATOS_*
     * create a deactivate function
     * run 'hash -r' to help autocompletion
     * change PS1 to print '(ATOS)'
    
    When calling deactivate the previous state is restored.

--- setenv.sh
+++ setenv.sh
@@ -7,5 +7,49 @@
 #   make all
 #
 
+deactivate() {
+  # Restore old values
+  if [ -n $"_OLD_ATOS_PATH" ]; then
+    PATH="$_OLD_ATOS_PATH"
+    export PATH
+    unset _OLD_ATOS_PATH
+  fi
+
+  if [ -n $"_OLD_ATOS_PS1" ]; then
+    PS1="$_OLD_ATOS_PS1"
+    export PS1
+    unset _OLD_ATOS_PS1
+  fi
+
+  if [ -n $"_OLD_ATOS_PYTHONPATH" ]; then
+    PYTHONPATH="$_OLD_ATOS_PYTHONPATH"
+    export PYTHONPATH
+    unset _OLD_ATOS_PYTHONPATH
+  fi
+
+  # Make bash and zsh forget about previous commands
+  # Overwise the last PATH change will not be taken into account
+  if [ -n "$BASH" -o -n "$ZSH_VERSION" ] ; then
+    hash -r
+  fi
+
+  # Self destroy
+  unset -f deactivate
+}
+
+
+# Save old values
+_OLD_ATOS_PATH="$PATH"
+_OLD_ATOS_PYTHONPATH="$PYTHONPATH"
+_OLD_ATOS_PS1="$PS1"
+
+# Export the new ones
 export PATH=$PWD/devimage/bin:$PATH
 export PYTHONPATH=$PWD/devimage/lib/python:$PYTHONPATH
+export PS1="(ATOS) $PS1"
+
+# Makes bash and zsh to forget about previous command and take PATH change into
+# account
+if [ -n "$BASH" -o -n "$ZSH_VERSION" ] ; then
+    hash -r
+fi
